ae548d000bea7ee8c0725228111d5298ca19d729
kristina.zagric <kristina.zagric@endava.com>
2021-11-27 11:03:29 +0100

    Initial commit

---
+++ src/test/java/steps/GSteps.java
@@ -0,0 +1,22 @@
+package steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import tests.BaseTest;
+
+import java.io.IOException;
+
+public class GSteps extends BaseTest {
+
+    @Before
+    public void initCucumber() {
+        init("Chrome", "95", 30);
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        reportScreenshot("Test", "TestAllure");
+        quitDriver();
+    }
+
+}
